71133f2e2158713b69d1b0c7fe28070b0df1b6ce
mumu <tnddls2ek@naver.com>
2021-02-15 16:57:35 +0900

    유저모델 정의

--- MukitList_Firebase/Model/User.swift
+++ MukitList_Firebase/Model/User.swift
@@ -10,19 +10,19 @@ import UIKit
 // 로그인 한 유저 정보 저장.  - core data or realm 이 아니라서 매번 로그인해야하는 번거로움이 있음 ,,ㅠ
 //일단 기능 부터 조지고 다 끝내고 core data , realm 조지기
 struct User {
-    var id : String
-    var name : String
-    var profileImage : String
+    var id : Dictionary<String,String>
+    var name : Dictionary<String,String>
+    var profileImage : Dictionary<String,String>
     
     mutating func fetchParameters(id : String, name : String, profileImage : String){
-        self.id = id
-        self.name = name
-        self.profileImage = profileImage
+        self.id = ["id":id]
+        self.name = ["name":name]
+        self.profileImage = ["profileImage":profileImage]
     }
     
     init(){
-        self.id = ""
-        self.name = ""
-        self.profileImage = ""
+        self.id = ["":""]
+        self.name = ["":""]
+        self.profileImage = ["":""]
     }
 }
