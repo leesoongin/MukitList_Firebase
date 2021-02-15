2725840ceb3c528426812a950d238f3a8ece938c
mumu <tnddls2ek@naver.com>
2021-02-15 18:04:25 +0900

    user model 재정의

--- MukitList_Firebase/Model/User.swift
+++ MukitList_Firebase/Model/User.swift
@@ -9,20 +9,25 @@ import UIKit
 
 // 로그인 한 유저 정보 저장.  - core data or realm 이 아니라서 매번 로그인해야하는 번거로움이 있음 ,,ㅠ
 //일단 기능 부터 조지고 다 끝내고 core data , realm 조지기
-struct User {
-    var id : Dictionary<String,String>
-    var name : Dictionary<String,String>
-    var profileImage : Dictionary<String,String>
+struct User : Codable{
+    var id : String
+    var name : String
+    var profileImage : String
     
+    var toDictionary : [String:Any] {
+        let dict : [String : Any] = ["id": id, "name":name, "profileImage":profileImage]
+        return dict
+    }
+        
     mutating func fetchParameters(id : String, name : String, profileImage : String){
-        self.id = ["id":id]
-        self.name = ["name":name]
-        self.profileImage = ["profileImage":profileImage]
+        self.id = id
+        self.name = name
+        self.profileImage = profileImage
     }
     
     init(){
-        self.id = ["":""]
-        self.name = ["":""]
-        self.profileImage = ["":""]
+        self.id = ""
+        self.name = ""
+        self.profileImage = ""
     }
 }
