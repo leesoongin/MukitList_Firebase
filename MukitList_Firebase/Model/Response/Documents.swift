82d98523e96522407c8efff0c7cf93bb702733db
mumu <tnddls2ek@naver.com>
2021-02-19 21:32:30 +0900

    먹킷리스트 뷰 , 뷰모델 구성, 가게정보 Document to Dictionary 프로퍼티 추가

--- MukitList_Firebase/Model/Response/Documents.swift
+++ MukitList_Firebase/Model/Response/Documents.swift
@@ -19,4 +19,9 @@ struct Document : Codable {
     let id : String
     let x : String
     let y : String
+    
+    var toDictionary : [String : Any]{
+        let dict : [String: Any] = ["place_name":place_name, "address_name":address_name, "category_group_name":category_group_name, "category_group_code":category_group_code, "id":id, "x":x, "y":y]
+        return dict
+    }
 }
