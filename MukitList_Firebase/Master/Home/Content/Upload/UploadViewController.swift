9fc070e76dfdddcb7ddc55ce227c77098481e96d
mumu <tnddls2ek@naver.com>
2021-02-20 16:31:31 +0900

    카메라, 사진앨범 접근권한 추가, ImagePicker기능 추가

--- MukitList_Firebase/Master/Home/Content/Upload/UploadViewController.swift
+++ MukitList_Firebase/Master/Home/Content/Upload/UploadViewController.swift
@@ -13,19 +13,41 @@ class UploadViewController: UIViewController {
     @IBOutlet weak var titleLabel: UITextField!
     @IBOutlet weak var priceLabel: UITextField!
     @IBOutlet weak var photoLabel: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     
     let firebaseManager = FirebaseManager.shared
     let userViewModel = UserViewModel()
     let reviewViewModel = ReviewViewModel.shared
-    var placeId : String!
     
+    var placeId : String!
+    let picker = UIImagePickerController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.delegate = self
+        
         NotificationCenter.default.addObserver(self, selector: #selector(fetchId), name: NSNotification.Name("placeId"), object: nil)
-
+        
    }
     @objc func fetchId(notification : NSNotification){
         placeId = notification.userInfo!["placeId"] as? String
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+        self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func uploadCancel(_ sender: Any) {
@@ -41,4 +63,31 @@ class UploadViewController: UIViewController {
             }
         } //save
     }//upload
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: false, completion: nil)
+    }
+
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        }else{
+            print("Camera not available")
+        }
+    }
+}
+
+extension UploadViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print("image -- > \(image)")
+            imageView.image = image
+        }
+        print("info --> \(info)")
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
